bd1eef1c86173db0a1e47fe58b8bae51195cd451
juliocastellonmendoza <juliocastellonmendoza@gmail.com>
2023-06-30 21:57:47 -0500

    La suma total del carrito

--- src/main/java/com/web/librery/controller/HomeController.java
+++ src/main/java/com/web/librery/controller/HomeController.java
@@ -1,15 +1,17 @@
 package com.web.librery.controller;
 
+import com.web.librery.domain.DetalleOrden;
 import com.web.librery.domain.Libro;
+import com.web.librery.domain.Orden;
 import com.web.librery.service.LibroService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -19,6 +21,12 @@ public class HomeController {
     private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
     private LibroService libroService;
+
+    //Para almacenar los detalles de la orden
+    private List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    //datos de la orden
+    Orden orden = new Orden();
 
     public HomeController(LibroService libroService){
         this.libroService=libroService;
@@ -38,5 +46,33 @@ public class HomeController {
         libro = libroOptional.get();
         model.addAttribute("libro", libro);
         return "usuario/libroHome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(@RequestParam Long id, @RequestParam int cantidad,Model model){
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Libro libro = new Libro();
+        double sumaTotal=0;
+
+        Optional<Libro> optionalLibro = libroService.get(id);
+        LOGGER.info("Libro añadido: {}",optionalLibro.get());
+        LOGGER.info("Cantidad: {}",cantidad);
+        libro = optionalLibro.get();
+
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(libro.getPrecio());
+        detalleOrden.setNombre(libro.getTitulo());
+        detalleOrden.setTotal(libro.getPrecio()*cantidad);
+        detalleOrden.setLibro(libro);
+
+        detalles.add(detalleOrden);
+
+        sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart",detalles);
+        model.addAttribute("orden",orden);
+
+        return "usuario/carrito";
     }
 }
